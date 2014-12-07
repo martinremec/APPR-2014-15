73ddb50c64fcf72b5d98785f9fc0b87a81b48b95
Martin Remec <martin.remec@gmail.com>
2014-12-07 21:35:33 +0100

    Oddaja druge faze projekta. Uvoz tabel in grafi za obdobje 2008-2012.

--- projekt.r
+++ projekt.r
@@ -1,10 +1,11 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("uvoz/uvoz.r")
+source("branjePodatkov/energy_stats_2008_2012.r")
+source("grafi.r")
 
 # 3. faza: Analiza in vizualizacija podatkov
-source("vizualizacija/vizualizacija.r")
+# source("vizualizacija/vizualizacija.r")
 
 # 4. faza: Napredna analiza podatkov
-source("analiza/analiza.r")
+# source("analiza/analiza.r")
 
-cat("Končano.\n")
+# cat("Končano.\n")
--- projekt1.r
+++
@@ -1,8 +0,0 @@
-
-
-
-source("branjePodatkov/energy_stats_2008_2012.r")
-source("grafi.r")
-
-
-
