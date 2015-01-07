c47b8d6d672e83dbcc7bff3e5b6dc3c6ebc0bb38
Martin Remec <martin.remec@gmail.com>
2015-01-07 12:06:29 +0100

    uvoz in izdelava zemljevida

--- projekt.r
+++ projekt.r
@@ -1,9 +1,9 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("branjePodatkov/energy_stats_2008_2012.r")
-source("grafi.r")
+source("branjePodatkov/energy_stats_2008_2012.r", encoding = "UTF-8")
+source("grafi.r", encoding = "UTF-8")
 
 # 3. faza: Analiza in vizualizacija podatkov
-# source("vizualizacija/vizualizacija.r")
+source("vizualizacija/vizualizacija.r", encoding = "UTF-8")
 
 # 4. faza: Napredna analiza podatkov
 # source("analiza/analiza.r")
