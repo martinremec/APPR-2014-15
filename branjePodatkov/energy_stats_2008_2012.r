c7b6e0ab6e57908a8f7e2c9d2a8ed7de68f41997
Martin Remec <martin.remec@gmail.com>
2015-03-05 21:00:37 +0100

    Uvoz tabele z normiranimi podatki za 4. fazo.

--- branjePodatkov/energy_stats_2008_2012.r
+++ branjePodatkov/energy_stats_2008_2012.r
@@ -8,5 +8,5 @@ energy_stats_2009 <- branje.csv("CSVs/2009.csv", "Windows-1250", ";")
 energy_stats_2010 <- branje.csv("CSVs/2010.csv", "Windows-1250", ";")
 energy_stats_2011 <- branje.csv("CSVs/2011.csv", "Windows-1250", ";")
 energy_stats_2012 <- branje.csv("CSVs/2012.csv", "Windows-1250", ";")
-
+total_energy_production_1992_2012 <- branje.csv("CSVs/Total_Primary_Energy_Production_(Million_Btu).csv", "Windows-1250", ";")
 
